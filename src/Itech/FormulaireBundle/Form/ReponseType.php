af47c9a0bd8f27cad193b033e3ad125c5ba45a89
tomphi <thomas.philippe5555@gmail.com>
2015-06-02 11:40:02 +0200

    Version 1.1 : FOSUserBundle before Step 4

---
+++ src/Itech/FormulaireBundle/Form/ReponseType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Itech\FormulaireBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ReponseType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('note')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Itech\FormulaireBundle\Entity\Reponse'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'itech_formulairebundle_reponse';
+    }
+}
